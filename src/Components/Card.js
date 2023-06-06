1e0e1d6b4b24fd70aad34b94e7d0dd5d755c1fb7
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-06-06 18:53:17 +0300

    Device Friendly Solution

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,3 +1,8 @@
+import Icon1 from '../../src/icon-supervisor.svg';
+import Icon2 from '../../src/icon-team-builder.svg';
+import Icon3 from '../../src/icon-karma.svg';
+import Icon4 from '../../src/icon-calculator.svg';
+
 import classes from './Card.module.css';
 
 const Card = () => {
@@ -10,21 +15,30 @@ const Card = () => {
         successful
       </p>
       <div className={classes.firstCard}>
+        <div className={classes.cyanLine}></div>
         <h3>Supervisor</h3>
         <p className={classes.description}>Monitors activity to identify project roadblocks</p>
+        <img src={Icon1} className={classes.icon} />
       </div>
       <div className={classes.secondCard}>
+        <div className={classes.redLine}></div>
         <h3>Team Builder</h3>
         <p className={classes.description}>Scans our talent network to create the optimal team for your project</p>
+        <img src={Icon2} className={classes.icon} />
       </div>
-      <div className={classes.firstCard}>
+      <div className={classes.thirdCard}>
+        <div className={classes.orangeLine}></div>
         <h3>Karma</h3>
         <p className={classes.description}>Regularly evaluates our talent to ensure quality</p>
+        <img src={Icon3} className={classes.icon} />
       </div>
-      <div className={classes.firstCard}>
+      <div className={classes.fourthCard}>
+        <div className={classes.blueLine}></div>
         <h3>Calculator</h3>
         <p className={classes.description}>Uses data from past projects to provide better delivery estimates</p>
+        <img src={Icon4} className={classes.icon} />
       </div>
+      <div className={classes.whiteSpace}></div>
     </div>
   );
 };
